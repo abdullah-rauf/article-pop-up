405e1bda43a9db65332835beaf286b8a4348651e
Abdullah <abdullahrauf009@gmail.com>
2024-07-31 14:02:43 +0500

    project done

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleNewsLetter() {
+
+    const newsLetter = document.getElementById("newsLetter");
+
+    newsLetter.classList.toggle('active');
+
+
+}
+
+
+// const newsletter = document.getElementById('newsLetter')
+
+// newsletter.addEventListener('click', function(){
+//     newsletter.classList.toggle("active")
+// })
